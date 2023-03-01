69bb2f30cc4d559078c02dcd049b46f5e09721c9
pherzxc@gmail.com <pherzxc@gmail.com>
2023-02-28 21:20:50 -0700

    adding circumference method

--- Area.cs/Area of circl.cs
+++ Area.cs/Area of circl.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 public class HelloWorld
 {
@@ -9,9 +9,21 @@ public class HelloWorld
         var area = CircleArea(radius);
         
         Console.WriteLine ("The area circle:" +area);
+
+        calcCircumference(radius);
     }
     public static double CircleArea (double radius){
         return Math.PI *radius *radius;
     }
+
+    // John Code Here
+    private static void calcCircumference(int radius)
+    {
+        double convertedRadius = Convert.ToDouble(radius);
+        double pi = Math.PI;
+        double area = 2 * pi * radius;
+        Console.WriteLine("The Circumference (C=2πr) of your circle is: {0:F2}", area);
+
     }
+}
     
